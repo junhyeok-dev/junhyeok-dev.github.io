6254d8b0b3898d1af9dcc3843d6dae66d66350d7
Junhyeok Yun <junhyeok.dev@gmail.com>
2020-10-06 15:30:58 +0900

    feat: Add ajax load for various documents

--- js/loaddocs.js
+++ js/loaddocs.js
@@ -1,10 +1,3 @@
 $(window).on("load", () => {
-    let menus = $("#menu > li");
-    let docs = ["profile", "publications"];
-
-    for (var i = 0; i < menus.length; i++) {
-        menus[i].on("click", (i) => {
-            $("#content").load("docs/" + docs[i] + ".html")
-        })
-    }
-});
+    $("#content").load("docs/home.html");
+})
